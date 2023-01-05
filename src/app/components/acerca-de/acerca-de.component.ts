7a159421792729bd50f9f75a550e98e9eb8e62b5
Fonseca Roman <romanfonseca630@gmail.com>
2023-01-05 12:39:19 -0300

    Se realizo modificaciones en component'acerca-de'

--- src/app/components/acerca-de/acerca-de.component.ts
+++ src/app/components/acerca-de/acerca-de.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from 'src/app/model/Person.model';
 import { PersonService } from 'src/app/service/Person.service';
+import { TokenService } from 'src/app/service/token.service';
 
 
 @Component({
@@ -12,10 +13,23 @@ export class AcercaDeComponent implements OnInit {
 
   person:Person = new Person("","","","");
 
-  constructor(public personService:PersonService) { }
+  constructor(public personService:PersonService, private tokenService:TokenService) { }
+
+  isLogged = false;
 
   ngOnInit(): void {
-    this.personaService.getPersonas().subscribe(data =>{this.persona = data})
+    this.loadPerson();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    }else{
+      this.isLogged = false;
+    }
+  }
+
+  loadPerson(){
+    this.personService.detail(1).subscribe(data =>{
+      this.person = data
+    })
   }
 
 }
