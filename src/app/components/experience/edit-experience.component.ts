9d0bebfdf4e78b3e360a7772241d570004fe4996
Fonseca Roman <romanfonseca630@gmail.com>
2023-01-10 11:34:39 -0300

    Se implemento metodo ngOnInit y onUpdate en EditExperienceComponent

--- src/app/components/experience/edit-experience.component.ts
+++ src/app/components/experience/edit-experience.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@Angular/router';
+import { Experience } from 'src/app/model/experience';
+import { SExperienceService } from 'src/app/service/s-experience.service';
 
 @Component({
   selector: 'app-edit-experience',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditExperienceComponent implements OnInit {
 
-  constructor() { }
+  experience!:Experience;
+
+  constructor(private s_experience:SExperienceService, private activatedRouter:ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.s_experience.detail(id).subscribe(
+      data =>{
+        this.experience = data;
+      }, err => {
+        alert("Error no se puede cargar experiencia");
+        this.router.navigate(['']);
+      }
+    )
+  }
+
+  onUpdate():void{
+    const id = this.activatedRouter.snapshot.params['id'];
+    this.s_experience.update(id, this.experience).subscribe(
+      data =>{
+        this.router.navigate([''])
+      }, err => {
+        alert("Error no se puede modificar experiencia");
+        this.router.navigate(['']);
+      }
+    )
   }
 
 }
