a612087ebd8e9c1649da68d28a519005e38d0ed3
Fonseca Roman <romanfonseca630@gmail.com>
2023-01-09 22:32:58 -0300

    Cambios en experience.component.ts

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Experience } from 'src/app/model/experience';
+import { SExperienceService } from 'src/app/service/s-experience.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -6,11 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent implements OnInit {
-  experience:ExperienceComponent[]=[];
+  experience:Experience[]=[];
 
-  constructor() { }
+  constructor(private s_experience: SExperienceService, private tokenService:TokenService) { }
+
+  isLogged=false;
 
   ngOnInit(): void {
+    this.loadExperience();
+    if(this.tokenService.getToken()){
+      this.isLogged=true;
+    }else{
+      this.isLogged=false;
+    }
+  }
+
+  loadExperience():void{
+    this.s_experience.list().subscribe(data =>{this.experience = data;})
   }
 
 }
